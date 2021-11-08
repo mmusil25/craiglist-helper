f3580c254ba71d1c09ce41a49d36133cdeab3282
Mark Musil <musilmark25@gmail.com>
2021-11-08 05:03:40 -0500

    Updates

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-from flask import Flask
-from flask import render_template
-from flaskwebgui import FlaskUI # import FlaskUI
+from flask import escape
 
-app = Flask(__name__)
-ui = FlaskUI(app, width=500, height=500) # add app and parameters
+def hello_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
 
-
-@app.route("/")
-def hello():
-    return render_template('index.html')
-
-@app.route("/home", methods=['GET'])
-def home():
-    return render_template('some_page.html')
-
-
-if __name__ == "__main__":
-    # app.run() for debug
-    ui.run()
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return 'Hello {}!'.format(escape(name))
